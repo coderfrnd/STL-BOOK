fcd97a0fcc7ffc251a82bad234d75217f12f318f
attacker frnd <warriorsshadow39@gmail.com>
2024-06-11 00:13:19 +0530

    aarambh

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
-// import Check from './background/Check'
+
 
 function App() {
   
 
   return (
     <div className=''>
-    <div className='h-full w-full'>
-       
-       {/* <Check/> */}
-       <h1 className='text-white'>YES</h1>
-       <input type="text" className='border 2px white' placeholder='text' />
-       {/* <input type="text" /> */}
-    </div>
+      
       
      
     </div>
