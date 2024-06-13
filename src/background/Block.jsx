6578dd0d02afc79f640180807a73897b61c77368
attacker frnd <warriorsshadow39@gmail.com>
2024-06-13 15:26:34 +0530

    Correcting App.css

--- src/background/Block.jsx
+++ src/background/Block.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Block = ({ label }) => {
   return (
     <>
-      <button className="button font-bold rounded-xl p-4 w-[50%] my-3">
+      <button className="button font-bold rounded-xl p-4 w-[50%] my-3 bg-[#9AE9FA]">
         {label}
       </button>
     </>
--- src/background/Front_Btn.jsx
+++ src/background/Front_Btn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Front_Btn = ({label}) => {
   return (
-     <button className="button font-bold rounded-b-2xl p-4">{label}</button>
+     <button className="button font-bold rounded-b-2xl p-4 bg-[#9AE9FA]">{label}</button>
   )
 }
 
