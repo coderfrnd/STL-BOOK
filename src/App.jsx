7c3390b9a6ca46d51e35551734f65f9b4cdd9bea
attacker frnd <warriorsshadow39@gmail.com>
2024-06-17 00:48:36 +0530

    Routing in Extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Start from './Background/Start';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route,MemoryRouter } from 'react-router-dom';
 import Cppfront from './Languages/CPP/Cppfront';
 import JavaFront from './Languages/JAVA/JavaFront';
 import PythontFront from './Languages/Python/PythontFront';
@@ -8,7 +8,7 @@ import PythontFront from './Languages/Python/PythontFront';
 // Cppfront
 function App() {
   return (
-    <BrowserRouter>
+    <MemoryRouter>
     {/* <Start/> */}
     <Routes>
       <Route path="/" element={<Start/>} />
@@ -16,7 +16,7 @@ function App() {
           <Route path="/java" element={<JavaFront/>} />
           <Route path="/python" element={<PythontFront/>} />
         </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
    
   //  <Start
   );
