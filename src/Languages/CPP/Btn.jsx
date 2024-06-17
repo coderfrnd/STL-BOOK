403e85f7cdf1631106d9135bbae58b479924d186
Keshav Kartik Sharma <keshav4762@gmail.com>
2024-06-18 00:15:49 +0530

    back button

--- src/Languages/CPP/Btn.jsx
+++ src/Languages/CPP/Btn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Btn({lable}) {
   return (
     <div className='px-4'>
-        <div>
+        <div className='p-0 m-0'>
             <button className='flex justify-center items-center button rounded-2xl p-2 my-2 w-20 bg-[#9AE9FA]'>{lable}</button>
         </div>
     </div>
--- src/Languages/CPP/Cppfront.jsx
+++ src/Languages/CPP/Cppfront.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import Btn from './Btn'
+import React from "react";
+import Btn from "./Btn";
 // import { Link } from 'react-router-dom';
 const Cppfront = () => {
   return (
-    <div className='m-0 p-0'>
-      <div className='flex items-center justify-center'>
-        <Btn lable={"Map"} />
+    <div className="bg-blue-50">
+      <div className="m-0 p-0">
+        <div className="flex items-center justify-center">
+          <Btn lable={"Map"} />
+        </div>
+
+        <div className="flex justify-between">
+          <Btn lable={"Queue"} />
+          <Btn lable={"Array  "} />
+        </div>
+
+        <div className="flex justify-between">
+          <Btn lable={"Set"} />
+          <Btn lable={"Loop"} />
+        </div>
+
+        <div className="flex justify-between">
+          <Btn lable={"Stack"} />
+          <Btn lable={"String"} />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <Btn lable={"Vector"} />
+        </div>
       </div>
 
-      <div className='flex justify-between'>
-        <Btn lable={"Queue"} />
-        <Btn lable={"Array  "} />
-      </div>
-
-      <div className='flex justify-between'>
-        <Btn lable={"Set"} />
-        <Btn lable={"Loop"} />
-      </div>
-
-      <div className='flex justify-between'>
-        <Btn lable={"Stack"} />
-        <Btn lable={"String"} />
-      </div>
-
-      <div className='flex items-center justify-center'>
-        <Btn lable={"Vector"} />
+      <div className="p-0 m-0">
+        <button className="bg-blue-500 rounded-md text-white text-xs p-1">Back</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cppfront
+export default Cppfront;
